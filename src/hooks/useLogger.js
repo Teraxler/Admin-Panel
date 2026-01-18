2c5fc7771f39db937e7c427d608cf974e06ca70d
Mahdi <mahdi.teraxler.mtr@gmail.com>
2026-01-18 11:00:33 +0330

    develop: develop useLogger hook

---
+++ src/hooks/useLogger.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+export const useLogger = (name, value) => {
+  useEffect(() => {
+    console.info(`[useLogger] ${name} Update = ${value}`);
+  }, [value]);
+};
