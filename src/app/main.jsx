1b15067f5ea629d57257899a74cf06c1d491074e
Mahdi <mahdi.teraxler.mtr@gmail.com>
2026-07-28 12:49:51 +0330

    refactor: move styles folder from assets to src

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Theme } from "@radix-ui/themes";
-import "@/assets/styles/global.css";
+import "@/styles/global.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
