ce09ecb8556b50743582d7e71ce25ad21c4c69ad
Mahdi <mahdi.teraxler.mtr@gmail.com>
2026-08-03 21:26:41 +0330

    refactor: refactor order util function

--- src/features/order/orderUtil.js
+++ src/features/order/orderUtil.js
@@ -1,9 +1,8 @@
-const claculateTotalPrice = (items) => {
-  return items?.length
-    ? items.reduce((prevValue, currentValue) => {
-        return prevValue + currentValue.unitPrice * currentValue.quantity;
-      }, 0)
-    : 0;
+const claculateTotalPrice = (items = []) => {
+  return items.reduce(
+    (total, item) => total + item.unitPrice * item.quantity,
+    0,
+  );
 };
 
 export { claculateTotalPrice };
